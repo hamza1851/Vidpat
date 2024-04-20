c05a5c7cf7635c0765dca54fdf13696eb7b449f4
hamza1851 <hamza.dev011@gmail.com>
2024-04-20 18:41:52 +0530

    created login, logout, update features and added  models

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,17 +1,27 @@
 import express from "express";
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit:"16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
+// http://localhost:8000/api/v1/users/register
 
 export { app };
--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,44 +1,40 @@
 // require('dotenv').config({path: './env'})
-// Replacing ES module imports with CommonJS require statements
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 dotenv.config({
-  path: ".env",
+  path: "./.env",
 });
 
 connectDB()
   .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running at port: ${process.env.PORT || 8000}`);
-    });
-
-    app.on("error", (error) => {
-      console.log("Oops Server error:", error);
+      console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     });
   })
   .catch((err) => {
-    console.log("MONGO db connection failed:", err);
+    console.log("MONGO db connection failed !!! ", err);
   });
 
 /*
 import express from "express"
 const app = express()
-
-(async () => {
+( async () => {
     try {
-        mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        app.on("error", (error)=>{
-            console.log("ERROR: ", error);
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
             throw error
         })
 
         app.listen(process.env.PORT, () => {
             console.log(`App is listening on port ${process.env.PORT}`);
         })
+
     } catch (error) {
-        console.log("ERROR: ", error);
-        throw error
+        console.error("ERROR: ", error)
+        throw err
     }
-})();
+})()
+
 */
